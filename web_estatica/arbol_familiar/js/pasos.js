867fb543d960d0711d0e80bbae1c9ae1688d8ff2
aguilaral24 <aguilaral24@gmail.com>
2024-04-28 22:12:59 -0600

    Se agregan descripciones e introducción de defunciones, población y adopciones, además se modifica pobreza a población indígena

--- web_estatica/arbol_familiar/js/pasos.js
+++ web_estatica/arbol_familiar/js/pasos.js
@@ -375,4 +375,29 @@ function checkboxChanged() {
     main();
 }
 
+/*MOdal*/
+
+const overlay = document.getElementById('overlay');
+const closeButtons = document.getElementsByClassName('closeModal');
+const openModal = document.getElementById('openModal');
+
+
+for (let i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener('click', () => {
+        overlay.style.opacity = '0'; // Cambiar la opacidad del overlay a 0
+        setTimeout(() => {
+            overlay.style.display = 'none'; 
+            openModal.style.display = 'block'
+        }, 500);
+    });
+}
+
+openModal.addEventListener('click', () => {
+    overlay.style.opacity = '1'; 
+        setTimeout(() => {
+            overlay.style.display = 'block';
+            openModal.style.display = 'none'
+        }, 50);
+});
+
 
--- web_estatica/defunciones/resources/js/main.js
+++ web_estatica/defunciones/resources/js/main.js
@@ -1383,4 +1383,30 @@ function getRadioState() {
 }
 
 
+/*MOdal*/
+
+const overlay = document.getElementById('overlay');
+const closeButtons = document.getElementsByClassName('closeModal');
+const openModal = document.getElementById('openModal');
+
+
+for (let i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener('click', () => {
+        overlay.style.opacity = '0'; // Cambiar la opacidad del overlay a 0
+        setTimeout(() => {
+            overlay.style.display = 'none'; 
+            openModal.style.display = 'block'
+        }, 500);
+    });
+}
+
+openModal.addEventListener('click', () => {
+    overlay.style.opacity = '1'; 
+        setTimeout(() => {
+            overlay.style.display = 'block';
+            openModal.style.display = 'none'
+        }, 50);
+});
+
+
 
